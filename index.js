f34571cb70b5e72bf68a91febeb40e7e65029809
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-03-05 03:18:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 function theBeatlesPlay (musiciansArray, instrumentsArray){
   let emptyArray = [];
   for (let i = musiciansArray[0]; i < musiciansArray.length; i++) {
-    emptyArray.push(`${musiciansArray[i] plays ${instrumentsArray[i]}')
+    emptyArray.push(`${musiciansArray[i] plays ${instrumentsArray}`)
+  }
+}
+    
 
